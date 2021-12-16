818be1027e6d25aee4dc0b50e018948117549468
Dinorah Abrego <dinoabrego@Dinorahs-MacBook-Air.local>
2021-12-16 12:14:17 -0600

    Read Me file modififed. Local Storage function working again properly. Color changing still not functional

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# day-scheduler
+# Day Scheduler
+
+## Description
+
+This is a simple application that allows the user to plan their work day. The user can input their activities by each hour from 9 to 5 and then click save and have them stored in local storage so they can be retrieved even if they close the page and open it again. 
+
+The planner has a design that allows the user to see at first glance if an activity has passed, is active or is coming by showing hours in a past, present and future color coding.
+
+## Website Appearance 
+
+![Website screenshot](./assets/images/scheduler.png)
+
+## [Published Site](https://dinoabrego.github.io/day-scheduler)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,164 +1,198 @@
+//Variables to reference DOM elements
+var textEl09 = document.getElementById('09');
+var textEl10 = document.getElementById('10');
+var textEl11 = document.getElementById('11');
+var textEl12 = document.getElementById('12');
+var textEl13 = document.getElementById('13');
+var textEl14 = document.getElementById('14');
+var textEl15 = document.getElementById('15');
+var textEl16 = document.getElementById('16');
+var textEl17 = document.getElementById('17');
+
 //Manages the display of today's date
-var today = moment();
-$("#current-day").text(today.format("dddd, MMM Do, YYYY"));
+let today = moment();
+var todayEl = document.getElementById("current-day");
+todayEl.textContent = (today.format("dddd, MMM Do, YYYY , h:mm a"));
 
-//Setting the event colors accprding to thecurrent hour
+//Upon load it will set the event colors according to the current hour
+//It will call the function to get stored events
+ 
 
-let currentHour =  moment().format("HH");
-var hourBlock = document.getElementsByClassName('events');
+window.onload = function setColorBlock () {
+   
+    var hourBlocksId = [
+        "09", 
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17"
+    ];
 
-function setColors() {
-   var blockHour = parseInt($(this).attr("id"));
-    if (blockHour < currentHour) {
-          hourBlock.addClass("past").remove("present, future");
-    } else if (blockHour === currentHour) {
-          hourBlock.addClass("present").remove("past, future");
-    } else if (blockHour > currentHour) {
-          hourBlock.addClass("future").remove("past, present");
-    }
+    let currentHour =  moment().format('HH');
+    console.log(currentHour); 
+
+    for (var i = 0; i < hourBlocksId.length; i++) {
+        var hourBlocksEl = document.getElementById(hourBlocksId[i]);
+        let blockHour = hourBlocksEl.getAttribute('data-hour',[i]);
+        console.log(blockHour);
+        let blockHour2 = parseInt(hourBlocksEl.getAttribute('data-hour'));
+        console.log(blockHour2);
+        if (blockHour < currentHour) {
+            hourBlocksEl.addClass('past');
+        } else if (blockHour === currentHour) {
+            hourBlocksEl.addClass('present');
+        } else {
+            hourBlocksEl.addClass('future');
+        }
+    };
 }
 
-setInterval(setColors(), (1000 * 60) * 10);
 
 //Show previously saved events when page loads
-document.getElementById("09").onload = getStoredEvents9AM();
-document.getElementById("10").onload = getStoredEvents10AM();
-document.getElementById("11").onload = getStoredEvents11AM();
-document.getElementById("12").onload = getStoredEvents12PM();
-document.getElementById("13").onload = getStoredEvents1PM();
-document.getElementById("14").onload = getStoredEvents2PM();
-document.getElementById("15").onload = getStoredEvents3PM();
-document.getElementById("16").onload = getStoredEvents4PM();
-document.getElementById("17").onload = getStoredEvents5PM();
+textEl09.onload = getStoredEvents9AM();
+textEl10.onload = getStoredEvents10AM();
+textEl11.onload = getStoredEvents11AM();
+textEl12.onload = getStoredEvents12PM();
+textEl13.onload = getStoredEvents1PM();
+textEl14.onload = getStoredEvents2PM();
+textEl15.onload = getStoredEvents3PM();
+textEl16.onload = getStoredEvents4PM();
+textEl17.onload = getStoredEvents5PM();
+
 
 //Functions to retrieve saved events by hour
 function getStoredEvents9AM() {
-    var storedEvent9 = JSON.parse(localStorage.getItem("event9"));
-    if (storedEvent9 !== null) {
-        document.getElementById("09").value = storedEvent9; 
+    var storedEvent09 = JSON.parse(localStorage.getItem("event09"));
+    if (storedEvent09 !== null) {
+        textEl09.value = storedEvent09; 
     }
 }
 
 function getStoredEvents10AM() {
     var storedEvent10 = JSON.parse(localStorage.getItem("event10"));
     if (storedEvent10 !== null) {
-        document.getElementById("10").value = storedEvent10; 
+        textEl10.value = storedEvent10; 
     }
 }
 
 function getStoredEvents11AM() {
     var storedEvent11 = JSON.parse(localStorage.getItem("event11"));
     if (storedEvent11 !== null) {
-        document.getElementById("11").value = storedEvent11; 
+        textEl11.value = storedEvent11; 
     }
 }
 
 function getStoredEvents12PM() {
     var storedEvent12 = JSON.parse(localStorage.getItem("event12"));
     if (storedEvent12 !== null) {
-        document.getElementById("12").value = storedEvent12; 
+        textEl12.value = storedEvent12; 
     }
 }
 
 function getStoredEvents1PM() {
-    var storedEvent1 = JSON.parse(localStorage.getItem("event1"));
-    if (storedEvent1 !== null) {
-        document.getElementById("13").value = storedEvent1; 
+    var storedEvent13 = JSON.parse(localStorage.getItem("event13"));
+    if (storedEvent13 !== null) {
+        textEl13.value = storedEvent13; 
     }
 }
 
 function getStoredEvents2PM() {
-    var storedEvent2 = JSON.parse(localStorage.getItem("event2"));
-    if (storedEvent2 !== null) {
-        document.getElementById("14").value = storedEvent2; 
+    var storedEvent14 = JSON.parse(localStorage.getItem("event14"));
+    if (storedEvent14 !== null) {
+        textEl14.value = storedEvent14; 
     }
 }
 
 function getStoredEvents3PM() {
-    var storedEvent3 = JSON.parse(localStorage.getItem("event3"));
-    if (storedEvent3 !== null) {
-        document.getElementById("15").value = storedEvent3; 
+    var storedEvent15 = JSON.parse(localStorage.getItem("event15"));
+    if (storedEvent15 !== null) {
+        textEl15.value = storedEvent15; 
     }
 }
 
 function getStoredEvents4PM() {
-    var storedEvent4 = JSON.parse(localStorage.getItem("event4"));
-    if (storedEvent4 !== null) {
-        document.getElementById("16").value = storedEvent4; 
+    var storedEvent16 = JSON.parse(localStorage.getItem("event16"));
+    if (storedEvent16 !== null) {
+        textEl16.value = storedEvent16; 
     }
 }
 
 function getStoredEvents5PM() {
-    var storedEvent5 = JSON.parse(localStorage.getItem("event5"));
-    if (storedEvent5 !== null) {
-        document.getElementById("17").value = storedEvent5; 
+    var storedEvent17 = JSON.parse(localStorage.getItem("event17"));
+    if (storedEvent17 !== null) {
+        textEl17.value = storedEvent17; 
     }
 }
 
 
 //Saving to local storage
 
-var saveButton9 = document.getElementById('saveBtn9');
+var saveButton09 = document.getElementById('saveBtn09');
 var saveButton10 = document.getElementById('saveBtn10');
 var saveButton11 = document.getElementById('saveBtn11');
 var saveButton12 = document.getElementById('saveBtn12');
-var saveButton1 = document.getElementById('saveBtn1');
-var saveButton2 = document.getElementById('saveBtn2');
-var saveButton3 = document.getElementById('saveBtn3');
-var saveButton4 = document.getElementById('saveBtn4');
-var saveButton5 = document.getElementById('saveBtn5');
+var saveButton13 = document.getElementById('saveBtn13');
+var saveButton14 = document.getElementById('saveBtn14');
+var saveButton15 = document.getElementById('saveBtn15');
+var saveButton16 = document.getElementById('saveBtn16');
+var saveButton17 = document.getElementById('saveBtn17');
 
-saveButton9.onclick = saveEvent9AM();
-saveButton10.onclick = saveEvent10AM();
-saveButton11.onclick = saveEvent11AM();
-saveButton12.onclick = saveEvent12PM();
-saveButton1.onclick = saveEvent1PM();
-saveButton2.onclick = saveEvent2PM();
-saveButton3.onclick = saveEvent3PM();
-saveButton4.onclick = saveEvent4PM();
-saveButton5.onclick = saveEvent5PM();
+saveButton09.onclick = saveEvent9AM;
+saveButton10.onclick = saveEvent10AM;
+saveButton11.onclick = saveEvent11AM;
+saveButton12.onclick = saveEvent12PM;
+saveButton13.onclick = saveEvent1PM;
+saveButton14.onclick = saveEvent2PM;
+saveButton15.onclick = saveEvent3PM;
+saveButton16.onclick = saveEvent4PM;
+saveButton17.onclick = saveEvent5PM;
+
 
 function saveEvent9AM(){
-  var inputEvent9 = document.getElementById('09').value;
-  localStorage.setItem("event9", JSON.stringify(inputEvent9));
+  var inputEvent09 = textEl09.value;
+  localStorage.setItem("event09", JSON.stringify(inputEvent09));
 }
 
 function saveEvent10AM(){
-    var inputEvent10 = document.getElementById('10').value;
+    var inputEvent10 = textEl10.value;
     localStorage.setItem("event10", JSON.stringify(inputEvent10));
 }
 
 function saveEvent11AM(){
-  var inputEvent11 = document.getElementById('11').value;
+  var inputEvent11 = textEl11.value;
   localStorage.setItem("event11", JSON.stringify(inputEvent11));
 }
 
 function saveEvent12PM(){
-    var inputEvent12 = document.getElementById('12').value;
+    var inputEvent12 = textEl12.value;
     localStorage.setItem("event12", JSON.stringify(inputEvent12));
 }
 
 function saveEvent1PM(){
-    var inputEvent1 = document.getElementById('13').value;
-    localStorage.setItem("event1", JSON.stringify(inputEvent1));
+    var inputEvent13 = textEl13.value;
+    localStorage.setItem("event13", JSON.stringify(inputEvent13));
 }
 
 function saveEvent2PM(){
-    var inputEvent2 = document.getElementById('14').value;
-    localStorage.setItem("event2", JSON.stringify(inputEvent2));
+    var inputEvent14 = textEl14.value;
+    localStorage.setItem("event14", JSON.stringify(inputEvent14));
 }
 
 function saveEvent3PM(){
-    var inputEvent3 = document.getElementById('15').value;
-    localStorage.setItem("event3", JSON.stringify(inputEvent3));
+    var inputEvent15 = textEl15.value;
+    localStorage.setItem("event15", JSON.stringify(inputEvent15));
 }
 
 function saveEvent4PM(){
-    var inputEvent4 = document.getElementById('16').value;
-    localStorage.setItem("event4", JSON.stringify(inputEvent4));
+    var inputEvent16 = textEl16.value;
+    localStorage.setItem("event16", JSON.stringify(inputEvent16));
 }
 
 function saveEvent5PM(){
-    var inputEvent5 = document.getElementById('17').value;
-    localStorage.setItem("event5", JSON.stringify(inputEvent5));
+    var inputEvent17 = textEl17.value;
+    localStorage.setItem("event17", JSON.stringify(inputEvent17));
 }
